b35c3c0c6d3413d7820cfbcdc8dddbe0fc949ca4
philip-webdev <philisobank@gmail.com>
2024-12-04 18:17:58 +0100

    defi wallet

--- src/components/api.tsx
+++ src/components/api.tsx
@@ -1,4 +1,5 @@
  
+import axios from "axios";
 import Axios from "axios";
 import { useEffect, useState } from "react";
 import styled from "styled-components";
@@ -31,10 +32,10 @@ function Api() {
     // Fetching crypto data from the API only
     // once when the component is mounted
     useEffect(() => {
-        fetch(
+        axios.get(
              'https://twa-backend-g83o.onrender.com/api/cryptocurrency'
         )
-            .then((res) => res.json())
+            .then((res) => res.data)
             .then((result) => {
                 console.log(result);
                 GetName(result.data[0].name); // Assuming you're fetching the first cryptocurrency name
