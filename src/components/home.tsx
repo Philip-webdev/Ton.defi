fb78dbc995a428a56d557c66df16ce8872d78ba4
philip-webdev <philisobank@gmail.com>
2024-12-05 09:04:42 +0100

    defi wallet

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import Api from "./api";
 import styled from "styled-components";
-
+import { Button } from "./styled/styled";
+import 'react-icons/bs';
+import 'react-icons/fa';
+import { FaChartBar, FaDiscourse, FaGamepad, FaHome } from "react-icons/fa";
+import { BsFillWalletFill } from "react-icons/bs";
 
 const StyledApp = styled.div`
   background-color: #e8e8e8;
@@ -29,7 +33,7 @@ function home(){
             const slidex = ['1', '2'];
             currentIndex = (currentIndex + 1) % slidex.length;
             if (slideContents != null){
-            slideContents.style.transform = 'translateY(' + (currentIndex * -67.5) + 'px)';
+            slideContents.style.transform = 'translateX(' + (currentIndex * -67.5) + 'px)';
     
             }
         }
@@ -41,32 +45,43 @@ function home(){
         <StyledApp>
             <AppContainer>
                 <div>
-                    <div  id="header" style={{display:'flex', justifyContent:'space-between', margin:'0'}}><div><img src='https://i.imgur.com/yRr0m7B.png' height='14px' width='14px'/></div><div>My wallet</div><div><img src="https://i.imgur.com/FtIlp2H.png" height='14px' width='14px'/></div></div>
+                    <div  id="header" style={{display:'flex', justifyContent:'space-between', margin:'0'}}><div><img src='https://i.imgur.com/yRr0m7B.png' height='14px' width='14px'/></div><div>My wallet</div><div><img src="https://i.imgur.com/0DG5nGo.png" height='14px' width='14px'/></div></div>
                     <div id="showcase" style={{height:'100px', width: '100%', margin:'auto', marginTop:'5%',marginBottom:'5%', backgroundImage :'linear-gradient(to  right, #1FA0FF, #12DAFB, #A7FDCC)', borderRadius:'7px'}}></div>
 
                 <div style={{display:'flex',background :'white', justifyContent:'space-around', borderRadius:'7px'}}>
-                <div style={{borderRadius:'100%', padding:'10px'}}><a style={{textDecoration:'none'}} href='#/send'><img src="https://i.imgur.com/hCrmXO1.png" height='24px' width='24px'/></a></div>
-                <div style={{borderRadius:'100%', padding:'10px'}}><a style={{textDecoration:'none'}} href='#/buy'><img src="https://i.imgur.com/VDoLGZY.png" height='24px' width='24px'/></a></div>
-                <div style={{borderRadius:'100%', padding:'10px'}}><a style={{textDecoration:'none'}} href='#/swap'><img src="https://i.imgur.com/R8iA1Vo.png" height='24px' width='24px'/></a></div></div>
+                <div style={{borderRadius:'100%', background :'whitesmoke',padding:'10px'}}><a style={{textDecoration:'none'}} href='#/send'><img src="https://i.imgur.com/hCrmXO1.png" height='24px' width='24px'/></a></div>
+                <div style={{borderRadius:'100%', background :'whitesmoke', padding:'10px'}}><a style={{textDecoration:'none'}} href='#/buy'><img src="https://i.imgur.com/VDoLGZY.png" height='24px' width='24px'/></a></div>
+                <div style={{borderRadius:'100%', background :'whitesmoke', padding:'10px'}}><a style={{textDecoration:'none'}} href='#/swap'><img src="https://i.imgur.com/R8iA1Vo.png" height='24px' width='24px'/></a></div></div>
                    <br/>
 
-                <div style={{ margin:'auto', width:'100%', justifyContent:'center'}}>
-    <div  id='slideContainer' style={{zIndex:'0',marginLeft:'10px' , width:'auto', height:'fit-content', overflow:'hidden',  border:'none', borderRadius:'7px', justifySelf:'center'}}>
-        <div id='slideContents' style={{height:'fit-content', width:'auto',  transition:'1s ease'}}>
+                <div style={{ margin:'0', width:'100%', justifyContent:'center'}}>
+    <div  id='slideContainer' style={{zIndex:'0',marginLeft:'10px' , width:'100%', height:'fit-content', overflow:'hidden',  border:'none', borderRadius:'7px', justifySelf:'center'}}>
+        <div id='slideContents' style={{height:'fit-content', width:'auto', display:'flex',  transition:'1s ease'}}>
 {/* <img src='src/components/WhatsApp_Image_2024-11-29_at_07.00.35_1f4f8a03-removebg-preview.png' height='500px' width='auto'/> */}
 <img src='https://i.imgur.com/BbaNXnn.png' height='auto' width='auto' style={{borderRadius:'7px'}}/>
-<img src='https://i.imgur.com/UBMXOLK.png' height='auto' width='auto' style={{marginTop:'7px',borderRadius:'7px'}}/>
+<img src='https://i.imgur.com/UBMXOLK.png' height='auto' width='auto' style={{marginLeft:'7px',borderRadius:'7px'}}/>
         </div></div>
     
 </div> 
 <br/> <br/>
 <div>
-    <div style={{display :'flex', background:"white", alignContent:'center', borderRadius:'7px'}}><div style={{padding:'10px'}}>Tokens</div><div style={{color:"grey", padding:'10px'}}>NFTs</div></div>
+    <div style={{display :'flex', background:"white", alignContent:'center', borderRadius:'7px', width:'fit-content'}}><div style={{padding:'10px'}}>Tokens</div><div style={{color:"grey", padding:'10px'}}>NFTs</div></div>
 </div>
 <div> <br/> <br/>
     <Api/>
 </div>
                 </div>
+
+                <div style={{right:'0.1%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',background:'#e8e8e8', width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed', borderRadius:'7px'}}>
+            <a href='#/fav' style={{color:'black', textDecoration:'none'}}> 
+            <Button  style={{  fontFamily: 'Lexend' ,  marginLeft:'20px',bottom:'0%', marginRight:'27px', background:'none', color:"black"}}><img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />
+            <p style={{zoom:'80%'}}>Home</p> </Button></a>
+            <a href='#/Checkout' style={{color:'black', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'30px', background:'none', color:"black"}}><img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/>
+            <p style={{zoom:'80%'}}>wallet</p></Button></a>
+            <a href='#/orders' style={{color:'black', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'27px', background:'none', color:"black"}}> <img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> 
+              <p style={{zoom:'80%'}}>market</p></Button></a> <a href='#/task' style={{color:'black', textDecoration:'none'}}><Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"black"}}><img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>
+              <p style={{zoom:'80%'}}>Discover</p> </Button></a>
+            </div>
             </AppContainer>
         </StyledApp>
     )
