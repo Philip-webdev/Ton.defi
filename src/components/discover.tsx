c9791e5679cc702f631440cf370c2f63f3c6e268
Philip <106782051+Philip-webdev@users.noreply.github.com>
2024-12-14 14:00:50 +0100

    defi wallet

--- src/components/discover.tsx
+++ src/components/discover.tsx
@@ -51,6 +51,7 @@ function discover() {
 return(
     <StyledApp>
     <AppContainer>
+      <h1>Discover best blogs on Crypto</h1>
    <div id="blog_container" style={{display:'inline', margin:'auto', width:'50%', justifyContent:'center'}}>
    <a href="https://coinbound.io/best-crypto-blogs/"><div style={{background :'white', height:'300px', width:'300px',borderRadius:'10px'}}>
 </div></a><p>Best crypto blogs</p><br/>
