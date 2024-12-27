c85a028d72487b56eb70b83e1dd3533f01eb02fe
philip-webdev <philisobank@gmail.com>
2024-12-27 19:46:18 +0100

    defi

--- src/components/AccountRegistration.tsx
+++ src/components/AccountRegistration.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { Button } from "./styled/styled";
 import '../index.css';
 import * as multichainWallet from 'multichain-crypto-wallet';
+import { BsHouse, BsWallet2, BsShop, BsLightningCharge } from "react-icons/bs";
 const StyledApp = styled.div`
   background-color: #F9F9F9;
   color: black;
@@ -29,6 +30,16 @@ const AppContainer = styled.div`
   height:fit-content;
   margin: 0;
   font-family: Lexend ;
+`;
+
+const Icon = styled.div`
+background-color: white;
+   
+  
+ @media (prefers-color-scheme: dark) {
+     background-color: rgb(33,33,33);
+        color:grey;
+  }
 `;
   function Register() {
    
@@ -157,17 +168,18 @@ return(
     </form>
     </div> */}
     
-    <div style={{right:'0.1%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',background:'white', width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed', borderRadius:'7px'}}>
-            <a href='#/' style={{color:'black', textDecoration:'none'}}> 
-            <Button  style={{  fontFamily: 'Lexend' ,  marginLeft:'20px',bottom:'0%', marginRight:'27px', background:'none', color:"black"}}><img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />
-            <p style={{zoom:'80%'}}>Home</p> </Button></a>
-            <a href='#/send' style={{color:'black', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'30px', background:'none', color:"black"}}><img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/>
-            <p style={{zoom:'80%'}}>Wallet</p></Button></a>
-            <a href='#/market' style={{color:'black', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'27px', background:'none', color:"black"}}> <img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> 
-              <p style={{zoom:'80%'}}>Market</p></Button></a>
-               <a href='#/discover' style={{color:'black', textDecoration:'none'}}><Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"black"}}><img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>
-              <p style={{zoom:'80%'}}>Discover</p> </Button></a>
-            </div></div>
+   <Icon className="nav" style={{right:'0.1%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',  width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed' }}>
+               <a href='#/' style={{color:'grey', textDecoration:'none'}}> 
+               <Button  style={{  fontFamily: 'Lexend' ,  marginLeft:'20px',bottom:'0%', marginRight:'20px', background:'none', color:"grey"}}><BsHouse/>{/*<img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />*/}
+               <p style={{zoom:'80%'}}>Home</p> </Button></a>
+                <a href='#/send' style={{color:'grey', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'20px', background:'none', color:"grey"}}><BsWallet2/>{/*<img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/> */}
+               <p style={{zoom:'80%'}}>Wallet</p></Button></a>
+               <a href='#/market' style={{color:'grey', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%', marginRight:'20px', background:'none', color:"grey"}}><BsShop/> {/*<img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> */}
+                 <p style={{zoom:'80%'}}>Market</p></Button></a> 
+                 <a href='#/discover' style={{color:'grey', textDecoration:'none'}}>
+                 <Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"grey"}}><BsLightningCharge/>{/*<img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>*/}
+                 <p style={{zoom:'80%'}}>Discover</p> </Button></a>
+               </Icon> 
     </AppContainer></StyledApp>
 )
 }
