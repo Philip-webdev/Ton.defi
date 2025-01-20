a18bad282c834b1ae87069875cb99beabce1308c
philip-webdev <philisobank@gmail.com>
2025-01-20 18:26:44 +0100

    wallet

--- src/components/Buy.tsx
+++ src/components/Buy.tsx
@@ -56,18 +56,18 @@ return(
   <ExPanel  style={{display: 'flex'  , gap:'17px',padding:'20px', borderColor:'whitesmoke', borderBottomStyle:'groove', borderWidth:'1px', borderRadius:'10px'}}><img src='https://i.imgur.com/lP16Nne.png' height='20px' width='20px' style={{borderRadius:'100%'}} /><div  style={{marginLeft:'7px'}}><a href="https://ramp.alchemypay.org/#/index" style={{color:'grey', textDecoration:'none'}}>Alchemypay</a></div></ExPanel>
   </div>
 
-        <Icon className="nav" style={{right:'0.1%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',  width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed' }}>
-                  <a href='#/' style={{color:'grey', textDecoration:'none'}}> 
-                  <Button  style={{  fontFamily: 'Lexend' , bottom:'0%',  background:'none', color:"grey"}}><BsHouse/>{/*<img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />*/}
-                  <p style={{zoom:'100%'}}>Home</p> </Button></a>
-                   <a href='#/send' style={{color:'grey', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsWallet2/>{/*<img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/> */}
-                  <p style={{zoom:'100%'}}>Wallet</p></Button></a>
-                  <a href='#/market' style={{color:'grey', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsShop/> {/*<img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> */}
-                    <p style={{zoom:'100%'}}>Market</p></Button></a> 
-                    <a href='#/discover' style={{color:'grey', textDecoration:'none'}}>
-                    <Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"grey"}}><BsLightningCharge/>{/*<img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>*/}
-                    <p style={{zoom:'100%'}}>Discover</p> </Button></a>
-                  </Icon> 
+         <Icon className="nav" style={{right:'0.7%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',  width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed' }}>
+                            <a href='#/' style={{color:'grey', textDecoration:'none'}}> 
+                            <Button  style={{  fontFamily: 'Lexend' , bottom:'0%',  background:'none', color:"grey"}}><BsHouse/>{/*<img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />*/}
+                            <p style={{zoom:'100%'}}>Home</p> </Button></a>
+                             <a href='#/send' style={{color:'grey', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsWallet2/>{/*<img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/> */}
+                            <p style={{zoom:'100%'}}>Wallet</p></Button></a>
+                            <a href='#/market' style={{color:'grey', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsShop/> {/*<img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> */}
+                              <p style={{zoom:'100%'}}>Market</p></Button></a> 
+                              <a href='#/discover' style={{color:'grey', textDecoration:'none'}}>
+                              <Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"grey"}}><BsLightningCharge/>{/*<img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>*/}
+                              <p style={{zoom:'100%'}}>Discover</p> </Button></a>
+                            </Icon>
         
         </AppContainer>
     </StyledApp>
--- src/components/Send.tsx
+++ src/components/Send.tsx
@@ -143,18 +143,18 @@ const dropdown5 = () => {
         </FlexBoxCol>
         
 
-        <Icon className="nav" style={{right:'0.1%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',  width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed' }}>
-                  <a href='#/' style={{color:'grey', textDecoration:'none'}}> 
-                  <Button  style={{  fontFamily: 'Lexend' , bottom:'0%',  background:'none', color:"grey"}}><BsHouse/>{/*<img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />*/}
-                  <p style={{zoom:'100%'}}>Home</p> </Button></a>
-                   <a href='#/send' style={{color:'grey', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsWallet2/>{/*<img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/> */}
-                  <p style={{zoom:'100%'}}>Wallet</p></Button></a>
-                  <a href='#/market' style={{color:'grey', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsShop/> {/*<img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> */}
-                    <p style={{zoom:'100%'}}>Market</p></Button></a> 
-                    <a href='#/discover' style={{color:'grey', textDecoration:'none'}}>
-                    <Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"grey"}}><BsLightningCharge/>{/*<img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>*/}
-                    <p style={{zoom:'100%'}}>Discover</p> </Button></a>
-                  </Icon> 
+        <Icon className="nav" style={{right:'0.7%', bottom:'0%', display:'flex',justifyContent:'space-evenly' ,height:'fit-content',  width:'100%', paddingBottom:'10px', paddingRight:'10px',position:'fixed' }}>
+                    <a href='#/' style={{color:'grey', textDecoration:'none'}}> 
+                    <Button  style={{  fontFamily: 'Lexend' , bottom:'0%',  background:'none', color:"grey"}}><BsHouse/>{/*<img src='https://i.imgur.com/uxozY7V.png' height='14px' width='14px' />*/}
+                    <p style={{zoom:'100%'}}>Home</p> </Button></a>
+                     <a href='#/send' style={{color:'grey', textDecoration:'none'}}> <Button  style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsWallet2/>{/*<img src="https://i.imgur.com/hCrmXO1.png" height='14px' width='14px'/> */}
+                    <p style={{zoom:'100%'}}>Wallet</p></Button></a>
+                    <a href='#/market' style={{color:'grey', textDecoration:'none'}}>  <Button style={{  fontFamily: 'Lexend' ,bottom:'0%',  background:'none', color:"grey"}}><BsShop/> {/*<img src="https://i.imgur.com/loOhRv0.png" height='14px' width='14px' /> */}
+                      <p style={{zoom:'100%'}}>Market</p></Button></a> 
+                      <a href='#/discover' style={{color:'grey', textDecoration:'none'}}>
+                      <Button  style={{ fontFamily: 'Lexend' ,bottom:'0%', background:'none', color:"grey"}}><BsLightningCharge/>{/*<img src='https://i.imgur.com/S444rBc.png'height='14px' width='14px'/>*/}
+                      <p style={{zoom:'100%'}}>Discover</p> </Button></a>
+                    </Icon>
       </AppContainer>
     </StyledApp>
   );
