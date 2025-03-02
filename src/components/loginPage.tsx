e26a36c5092e66d3787e93742601f9c14b5cd90e
philip-webdev <philisobank@gmail.com>
2025-03-02 05:11:03 +0100

    defi

--- src/components/loginPage.tsx
+++ src/components/loginPage.tsx
@@ -111,17 +111,17 @@ const handleRegister = async () => {
                 <form style={{margin:'auto', width:'100%'}} onSubmit={(e) => { e.preventDefault(); }}>
                     <div>
                         <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" required style={{height:'30px',  width:'70%'}} />
+                        <input className='logs'  id="email" name="email" required style={{height:'30px',  width:'100%', background :'transparent', borderWidth:'1px', borderBlockStyle:'groove', borderColor:'gray', borderRadius:'3px'}} placeholder='@example.com'  />
                     </div>
                     <br></br>
                     <div>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required style={{height:'37px',  width:'72%'}}/>
+                        <input className='logs' type="password" id="password" name="password" required style={{height:'30px',  width:'100%', background :'transparent', borderWidth:'1px', borderBlockStyle:'groove', borderColor:'gray', borderRadius:'3px'}} placeholder='password'/>
                     </div>
                     <br></br>
-                    <div style={{display:'inline-flex', marginLeft:'37%', marginTop:'20px'}}>
-                    <button type="submit" onClick={handleLogin} >Login</button> <br></br><br />
-                    <button style={{margin:'7px'}} type="button" onClick={handleRegister}>Register</button></div>
+                    <div style={{display:'inline-flex', margin:'auto ', width:'80%', justifyContent:'space-between'}}>
+                    <button type="submit" onClick={handleLogin} style={{background:'greenyellow'}}>Login</button> <br></br><br />
+                    <button style={{margin:'0px'}} type="button" onClick={handleRegister}>Register</button></div>
                 </form>
             </div>
         </StyledApp>
