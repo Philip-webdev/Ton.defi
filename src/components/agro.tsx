b28d7b5c3ac75f2fafb621250c08191407698d86
philip-webdev <philisobank@gmail.com>
2025-07-02 22:01:54 +0100

    latest

--- src/components/agro.tsx
+++ src/components/agro.tsx
@@ -3,8 +3,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import React, { useEffect , useRef} from 'react';
 import styled from "styled-components";
 // @ts-ignore
-import { sRGBEncoding } from 'three/src/constants';
-
+  
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 const StyledApp = styled.div`
   background-color:  #F9F9F9;
   color: black;
@@ -74,32 +74,21 @@ const AgroApp = () => {
         const ambient = new THREE.AmbientLight(0xffffff, 0.5);
         scene.add(ambient);
 
-        const textureLoader = new THREE.TextureLoader();
-        // Load your plant texture image here (replace 'path/to/plant.png' with your image path)
-        const plantTexture = textureLoader.load('../49221.jpg');
-
-        (plantTexture as THREE.Texture).anisotropy = renderer.capabilities.getMaxAnisotropy();
-         
-        const width = 2, height = 3;
-        const segments = 32;
-        const geometry = new THREE.PlaneGeometry(width, height, segments);
-
-        const material = new THREE.MeshStandardMaterial({
-            map: plantTexture,
-            side: THREE.DoubleSide,
+        const Loader = new GLTFLoader();
+        let loadedScene: THREE.Object3D | null = null;
+            
+        Loader.load('https://github.com/Philip-webdev/campus-blessed-library-hub/raw/refs/heads/main/decorative-ceramic-vase.glb', function (gltf){
+            loadedScene = gltf.scene;
+            scene.add(gltf.scene);
         });
-
-        // Mesh
-        const plantMesh = new THREE.Mesh(geometry, material);
-        plantMesh.castShadow = true;
-        plantMesh.receiveShadow = true;
-        scene.add(plantMesh);
 
         let animationId: number;
 
         function animate() {
             animationId = requestAnimationFrame(animate);
-            plantMesh.rotation.y += 0.001; // Slow rotation for 3D effect
+            if (loadedScene) {
+                loadedScene.rotation.y += 0.001; // Slow rotation for 3D effect
+            }
             renderer.render(scene, camera);
         }
         animate();
