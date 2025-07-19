c2899beb0e828efad5c62801288d44c1a940357b
philip-webdev <philisobank@gmail.com>
2025-07-19 05:50:46 +0100

    early morning has gold

--- src/components/agro.tsx
+++ src/components/agro.tsx
@@ -3,7 +3,8 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import React, { useEffect , useRef} from 'react';
 import styled from "styled-components";
 // @ts-ignore
-  
+ import {Menu} from 'lucide-react'; 
+  import {User} from 'lucide-react'; 
 import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
 import FootNavig from './footnavig';
 
@@ -16,7 +17,7 @@ const StyledApp = styled.div`
       color: white ;
   }
   min-height: 250vh;
-  padding:10px;
+  padding:20px;
   margin:0;
   
    zoom :100%;
@@ -33,6 +34,10 @@ background-color: white;
      background-color: rgb(15,15,15);
         color:white;
   }
+`;
+ 
+const heder = styled.div`
+backdrop-filter:blur(30px);
 `;
 
 const AgroApp = () => { 
@@ -65,9 +70,9 @@ const AgroApp = () => {
         controls.enableDamping = true;
         controls.dampingFactor = 0.05;
         controls.enablePan = false;
-        
-        controls.maxDistance = 100;
-
+        controls.zoom0 = 0;
+        controls.zoomToCursor = false;
+       
         // Lighting
         const light = new THREE.DirectionalLight(0xffffff, 0.8);
         light.position.set(2, 5, 5);
@@ -126,21 +131,21 @@ const AgroApp = () => {
     return (
         <StyledApp>
           
-                <div style={{justifyContent:'space-between', display:'flex', top:'0', marginBottom:'7px', backdropFilter:'blur(30px)'}}>
-                    <div style={{left:'0'}}>Menu</div><div>Me</div>
-                </div>
-                <Acc_panel>
-                 <p style={{padding:'30px', textAlign:'center', marginTop:'10%', fontFamily:'helvetica',fontWeight:'900',fontSize:'30px'}}> $500.00</p>
+                <heder style={{justifyContent:'space-between', display:'flex', top:'0', marginBottom:'7px'}}>
+                 <Menu/><User/>
+                </heder>
+                <Acc_panel style={{ height:'150px'}}><p style={{color:'grey', fontSize:'small', textAlign:'center', padding:'7px'}}>Total Earnings</p>
+                 <p style={{ textAlign:'center',  fontFamily:'helvetica',fontWeight:'900',fontSize:'30px'}}> $500.00</p>
                     </Acc_panel>
                 <div
                     ref={mountRef}
-                    style={{ width: '100%',  overflow: 'hidden' ,zoom: '100%'}}
+                    style={{ width: '100%',  overflow: 'hidden' , margin:'10px'}}
                 />  
                 <div><FootNavig/></div>
 
-                <Acc_panel>this should be the panel that contains the timer</Acc_panel>
+                <Acc_panel style={{ width:'90%',position:'absolute' , top:'70%', textAlign:'center'}}><p style={{ marginTop:'7%',fontSize:'20px'}}>Ends: 20/07/25</p></Acc_panel>
                 <br/>
-                <Acc_panel> tshis should be the Acc_panel that contains the investment data  like the allocated timing, expected results</Acc_panel>
+                <Acc_panel style={{ width:'90%',position:'absolute' , top:'85%', textAlign:'center', fontSize:'30px'}}><p style={{ marginTop:'7%',fontSize:'20px'}}>%ROI: 10</p></Acc_panel>
          
         </StyledApp>
     );
